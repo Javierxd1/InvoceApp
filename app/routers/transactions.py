ce8fab6e03842c345c9449026cd327700cbf065d
Javier Agudelo <javieragu1104@gmail.com>
2024-12-13 00:04:54 -0500

    Cambios a transactions

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -5,7 +5,7 @@ from sqlmodel import select
 
 routerTransactions = APIRouter()
 
-@routerTransactions.post("/createTransactions", tags=['Transactions'])
+@routerTransactions.post("/createTransactions", status_code= status.HTTP_201_CREATED, tags=['Transactions'])
 async def create_transaction(transactions_data: TransactionsCreate, session:SessionDep):
     transactions_data_dict = transactions_data.model_dump() #Convierte en diccionario los datos de transacción de entrada
     customer = session.get(Customer, transactions_data_dict.get('customer_id')) #Busca el id del customer dentro de la base Customer
@@ -13,11 +13,11 @@ async def create_transaction(transactions_data: TransactionsCreate, session:Sess
     if not customer:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='El cliente no existe')
     
-    transaction_db = Transactions.model_validate(transactions_data_dict)
-    session.add(transaction_db)
-    session.commit()
-    session.refresh(transaction_db)
-    return transaction_db
+    transaction_db = Transactions.model_validate(transactions_data_dict) #Empleando el modelo de transacción, valida los datos recibidos
+    session.add(transaction_db) #Añade el diccionario a la base de datos
+    session.commit() #Cierra la sesión
+    session.refresh(transaction_db) #Refresca la sección
+    return transaction_db #Devuelve el valor
 
 @routerTransactions.get("/transactions", tags=['Transactions'])
 async def list_transaction(session:SessionDep):
