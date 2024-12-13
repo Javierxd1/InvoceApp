a0fd862af09295053ad49c9f16b7b56c6b9ad986
Javier Agudelo <javieragu1104@gmail.com>
2024-12-12 23:58:00 -0500

    Relación de muchos a muchos usando Plan

--- models.py
+++ models.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel,EmailStr
 from sqlmodel import SQLModel,Field,Relationship
 
+class CustomerPlan(SQLModel,table=True):
+    id: int = Field(primary_key=True)
+    plan_id: int = Field(foreign_key="plan.id")
+    customer_id : int = Field(foreign_key="customer.id")
+    
+
+class Plan(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    name: str = Field(default=None)
+    price: int = Field(default=None)
+    description: str = Field(default=None)
+    customers: list['Customer'] = Relationship(
+        back_populates="plans", link_model=CustomerPlan
+    )
+
 class CustomerBase(SQLModel):
     name: str = Field(default=None)
     description : str | None = Field(default=None)
@@ -16,6 +31,9 @@ class CustomerUpdate(CustomerBase):
 class Customer(CustomerBase, table = True):
     id: int | None = Field(default=None, primary_key=True)
     transactions: list["Transactions"] = Relationship(back_populates="customer")
+    plans: list[Plan] = Relationship(
+        back_populates='customers', link_model=CustomerPlan
+    )
 
 
 class TransactionsBase(SQLModel):
@@ -25,7 +43,7 @@ class TransactionsBase(SQLModel):
 class Transactions(TransactionsBase, table = True):
     id:int | None = Field(default=None, primary_key= True)
     customer_id: int = Field(foreign_key = "customer.id") #Crea la relación con la tabla Customer - aunque se define como customer
-    customer:Customer = Relationship(back_populates="transactions")
+    customer: Customer = Relationship(back_populates="transactions")
 
 class TransactionsCreate(TransactionsBase):
     customer_id: int = Field(foreign_key = "customer.id")
