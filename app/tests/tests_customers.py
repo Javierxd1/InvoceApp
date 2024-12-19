abbf63348c6d9e2b0eae7e658ae393613d997873
Javier Agudelo <javieragu1104@gmail.com>
2024-12-18 23:52:45 -0500

    Se crea un segundo test de lectura de datos desde un id

--- app/tests/tests_customers.py
+++ app/tests/tests_customers.py
@@ -9,5 +9,19 @@ def test_create_customer(client):
             "age":33
         }
     )
-
     assert response.status_code == status.HTTP_201_CREATED
+
+def test_read_customer(client):
+    response = client.post(
+        "/customers",
+        json = {
+            "name":"John Doe",
+            "email": "user@example.com",
+            "age":33
+        }
+    )
+    assert response.status_code == status.HTTP_201_CREATED
+    customer_id :int = response.json()["id"] #Permite acceder al id del usuario creado
+    response_read = client.get(f"/customers/{customer_id}")
+    assert response_read.status_code == status.HTTP_200_OK
+    assert response_read.json()["name"] == "John Doe"
